786fcee24b2087dd353ed9f52a2f9074c86f4d7f
BlueSkyFish <blueskyfish@kirchnerei.de>
2015-06-19 20:57:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,156 @@
+/*!
+ * Project: node-rest-header-session
+ * Filter the request header and if the middleware finds the token,
+ * then it append the "restSession" object to the request instance.
+ */
+
+// require external modules
+var
+  extend = require('extend'),
+  UUID = require('simple-uuid');
+
+// constants
+var
+  HEADER_NAME = 'x-rest-session-token',
+  METRICS_URL = '/metrics/rest-header';
+
+// default options
+var
+  defaultOptions = {
+    name: HEADER_NAME,
+    debug: true,
+    metrics: {
+      enable: true,
+      url: METRICS_URL
+    }
+  };
+
+// Cache for the rest header
+var
+  cache = {};
+
+
+// extends and prepares the options...
+function _prepareOptions (options) {
+  var
+    tempOptions = extend(false, {}, defaultOptions);
+
+  if (options) {
+    tempOptions = extend(false, tempOptions, options);
+  }
+
+  return tempOptions;
+}
+
+
+// initialize the middleware
+function _initialize(options) {
+
+  // adjust the configuration
+  var
+    headerName = options.name || HEADER_NAME;
+
+  // handle the request
+  return function handleRequest(req, res, next) {
+
+    var token, restSession;
+
+    if (req.restSession) {
+      if (options.debug) {
+        console.debug('rest session is already created');
+      }
+      next();
+      return
+    }
+
+    token = req.get(headerName);
+
+    if (!token) {
+      // create a new token
+      token = UUID();
+      if (options.debug) {
+        console.debug('create new token %s', token);
+      }
+    }
+    // update the session token
+    res.set(headerName, token);
+
+    // get the session
+    restSession = cache[token];
+
+    if (!restSession) {
+      // session is not available
+      restSession = cache[token] = {};
+      if (options.debug) {
+        console.log('create a restSession to %s', token);
+      }
+    }
+
+    // update the request object
+    req.restSession = restSession;
+
+    next();
+  };
+}
+
+// executes the metrics request.
+function _handleMetrics(options, req, res) {
+  var
+    count = 0,
+    data = {
+      tokens: []
+    };
+
+  if (options.debug) {
+    console.debug('GET: %s (metrics from rest session)', options.metrics.url);
+  }
+
+  for (var token in cache) {
+    if (!cache.hasOwnProperty(token)) {
+      continue;
+    }
+    data.tokens.push(token);
+    count++;
+  }
+
+  data.count = count;
+
+  res.send(data);
+}
+
+/**
+ * TBD
+ *
+ * @param {object} app the application from express.
+ * @param {object} options the configuration object.
+ */
+module.exports = function (app, options) {
+
+  var
+    thisOptions = _prepareOptions(options);
+
+  app.use(_initialize(thisOptions));
+
+  if (thisOptions.metrics && thisOptions.metrics.enable === true) {
+    // register the metrics handler
+
+    var
+      url = thisOptions.metrics.url;
+
+    if (!url) {
+      thisOptions.metrics.url = url = METRICS_URL;
+
+      if (thisOptions.debug) {
+        console.debug('adjust the metrics url to %s', METRICS_URL);
+      }
+    }
+
+    if (thisOptions.debug) {
+      console.debug('register the GET: %s', url);
+    }
+
+    app.get(url, function (req, res) {
+      _handleMetrics(thisOptions, req, res);
+    });
+  }
+};
---
+++ readme.md
@@ -0,0 +1,74 @@
+
+# Node Rest Header Session
+
+The middleware creates a simple session management via a HTTP header field that contains a token. This is for a stateless RESTful service.
+
+## Usage
+
+	var
+		express = require('express'),
+		bodyParser = require('body-parser),
+		restHeader = require('node-rest-header-session');
+
+	var
+		app = express();
+
+	// register the middleware and the metrics request.
+	restHeader(app, {
+		name: 'x-this-is-a-restful-header-field',
+		debug: true,
+		metrics: {
+			enable: true,
+			url: '/metrics/rest-header'
+		}
+	});
+
+	// every request
+	app.get('/', function (req, res) {
+		var count = req.restSession.count || 0;
+
+		req.restSession.count = ++count;
+
+		res.send({
+			count: count
+		});
+	});
+
+## Options
+
+The middleware will be configured with some options
+
+Name             | Kind     | Description
+-----------------|----------|----------------------------------------------
+name             | string   | The header name for the session management (**Default** `x-rest-session-token`).
+debug            | boolean  | Show debug messages with `console.debug` (**Default**: `true`).
+metrics.enable   | boolean  | Add a metrics url for the session values (**Default**: `false`).
+metrics.url      | string   | The url für the metrics information (**Default** `/metrics/rest-header`)
+
+
+
+
+
+## License
+
+	The MIT License (MIT)
+
+	Copyright (c) 2015 BlueSkyFish <blueskyfish@kirchnerei.de>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
