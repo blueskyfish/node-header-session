82c0b87dbc049cff61d2e08dfb65220099ba2af5
BlueSkyFish <blueskyfish@kirchnerei.de>
2015-06-23 11:34:13 +0200

    add demo application

---
+++ demo/demo.js
@@ -0,0 +1,37 @@
+/**
+ * Demo App for middleware "node-header-session"
+ */
+ 
+ 'use strict';
+ 
+var
+	express = require('express');
+
+var
+	headerSession = require('..');
+
+var
+	app = express();
+
+headerSession(app, {
+	name: 'x-demo-session-token',
+	debug: true
+});
+
+app.get('/count', function (req, res) {
+	
+	req.headerSession.getSession().then(function (session) {
+		console.log('count: receive session: ', session);
+		var count = session.count || 0;
+		session.count = ++count;
+		res.send({
+			count: count
+		});
+	});
+});
+
+app.listen(3000, function () {
+	console.log('demo server is startet and listen on http://localhost:3000');
+});
+
+	 
